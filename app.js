a393c3889618d22311f7674f85bbef96a73a4b0a
Stacy <raceystacy@gmail.com>
2020-05-08 10:43:44 -0600

    attempting to upload to heroku

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ const day = date.getDate();
 
 app.use(express.static('public'));
 
-/* app.use(function(req, res, next) {
+app.use(function(req, res, next) {
 	if (req.originalUrl && req.originalUrl.split('/').pop() === 'favicon.ico') {
 		return res.sendStatus(204);
 	}
 	return next();
 });
- */
+
 app.get('/', function(req, res) {
 	let newPageInput = '';
 	List.find({}, function(err, lists) {
